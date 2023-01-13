1a9e1ec2994a05baa698731d0ea9d9153a423b25
Anton Saltanov <saltanov.anton@yandex.ru>
2023-01-13 23:53:52 +0900

    task2

--- src/Transport/Transport.java
+++ src/Transport/Transport.java
@@ -18,4 +18,28 @@ public class Transport {
     public void end() {
         System.out.println(this.model + " остановился");
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getVolumeEngine() {
+        return volumeEngine;
+    }
+
+    public void setVolumeEngine(double volumeEngine) {
+        this.volumeEngine = volumeEngine;
+    }
 }
