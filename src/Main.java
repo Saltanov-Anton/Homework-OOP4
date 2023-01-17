c9e1c7ee265ed532274d6ba103c03e0c16dac8ac
Anton Saltanov <saltanov.anton@yandex.ru>
2023-01-17 16:33:01 +0900

    enum

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         Truck truckZil = new Truck("131", "ZIL", 6.0);
         Truck truckMaz = new Truck("6440 RR", "MAZ", 12.66);
 
-        Bus busPaz3203 = new Bus("3203","PAZ",4.5);
+        Bus busPaz3203 = new Bus("3203", "PAZ", 4.5);
         Bus busRoutemaster = new Bus("Routemaster", "AEC", 9.6);
         Bus busTc2000 = new Bus("TC/2000", "Blue Bird", 5.9);
         Bus bus5921 = new Bus("5921", "LiAZ", 9.3);
@@ -25,5 +25,7 @@ public class Main {
 
         System.out.println("Водитель " + driverVasya.getName() + " управляет автомодилем "
                 + carSkyline.getModel() + " и будет учавствовать в заезде");
+
+        
     }
 }
